f32db40f10fb53661a3ba1d14638c62a32425d73
unknown <cesargarcia9726@gmail.com>
2022-08-04 11:25:04 -0500

    [ADD] Navbar setup: font, mouse hover function, Routing

--- medmor_frontend/src/app/app-routing.module.ts
+++ medmor_frontend/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from '../app/components/home/home.component';
+import {LoginComponent} from '../app/components/login/login.component';
+import {ProductComponent} from '../app/components/product/product.component';
+import {RegisterComponent} from '../app/components/register/register.component';
+import {SectionComponent} from '../app/components/section/section.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'', component:HomeComponent},
+  {path:'login', component:LoginComponent},
+  {path:'product', component:ProductComponent},
+  {path:'register', component:RegisterComponent},
+  {path:'section', component:SectionComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- medmor_frontend/src/app/app.module.ts
+++ medmor_frontend/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProductComponent } from './components/product/product.component';
+import { SectionComponent } from './components/section/section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent,
+    ProductComponent,
+    SectionComponent
   ],
   imports: [
     BrowserModule,
